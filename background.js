bfadadbf4d3569d88cf695ed99e05b22c7d8f4c6
An Bast <sebastianrondonja@hotmail.com>
2023-05-14 01:34:40 -0500

    Id Page Destacada
    - Proyecto aparentemente pausado por que no es posible modificar la visualizacion de una pestaña (el tab), por medio de una extension

--- background.js
+++ background.js
@@ -6,6 +6,19 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "pageDestacada") {
-    console.log("El Id de la page destacada es:", tab.id);
+    if (!/^chrome:\/\//.test(tab.url)) {
+      console.log("El Id de la page destacada es:", tab.id);
+    } else {
+      chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "Error",
+        message: "Esta página no se puede destacar.",
+      });
+    }
   }
 });
+
+function setRedTitleColor() {
+  document.querySelector("title").style.color = "red";
+}
